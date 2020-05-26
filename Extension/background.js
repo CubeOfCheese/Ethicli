1ae1d623cd01ebced67137be28fd89d944ced7ad
Billy Hernandez <hernanbi@ecs.csus.edu>
2020-05-25 23:52:31 -0700

    cleaned up code

--- Extension/background.js
+++ Extension/background.js
@@ -3,16 +3,9 @@ chrome.browserAction.setIcon({path : {"16":"icons/gray_icon16.png"}})
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.shoppingPage == true) {
-      console.log("somehow true");
-      console.log(request.shoppingPage);
       chrome.browserAction.setIcon({path : {"16": "icons/get_started16.png"}})
 
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab :
-                  "from the extension");
-      console.log(sender.tab.title.split(' ')[0])
       var companyName = sender.tab.title.split(' ')[0];
-
       var blueSignRequest = new XMLHttpRequest()
       var url = 'http://localhost:8080/score/' + companyName;
       blueSignRequest.open('GET', url, true)
@@ -34,25 +27,3 @@ chrome.runtime.onMessage.addListener(
     }
     sendResponse({acknowledge: "okay"});
 });
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log(sender.tab ?
-//                 "from a content script:" + sender.tab :
-//                 "from the extension");
-//     console.log(sender.tab.title.split(' ')[0])
-//     var companyName = sender.tab.title.split(' ')[0];
-//
-//     var blueSignRequest = new XMLHttpRequest()
-//     var url = 'http://localhost:8080/score/' + companyName;
-//     blueSignRequest.open('GET', url, true)
-//     blueSignRequest.onload = function() {
-//       console.log(this.response);
-//       if (this.response == "true") {
-//         // chrome.pageAction.setBadgeText({text:"Yay"});
-//       }
-//       else {
-//         // chrome.pageAction.setBadgeText({text:"Poo"});
-//       }
-//     }
-//     blueSignRequest.send()
-//   });
--- Extension/content.js
+++ Extension/content.js
@@ -1,24 +1,14 @@
-var title = document.title;
-console.log(title);
 
 window.onload = function() {
   var dom = document.getElementsByTagName('html')[0].innerHTML;
-  console.log(dom);
   var present = dom.search("content=\"product\"")
   if (present == -1) {
     present = dom.search("gl-product-card-container")
   }
-  console.log(present);
   if (present != -1) {
-    console.log("found");
-    chrome.runtime.sendMessage({shoppingPage: true}, function(response) {
-      console.log(response.acknowledge);
-    });
+    chrome.runtime.sendMessage({shoppingPage: true}, function(response) {});
   }
   else {
-    console.log("not found");
-    chrome.runtime.sendMessage({shoppingPage: false}, function(response) {
-      console.log(response.acknowledge);
-    });
+    chrome.runtime.sendMessage({shoppingPage: false}, function(response) {});
   }
 };
