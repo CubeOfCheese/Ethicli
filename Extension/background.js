397cf6f800cd9211e00c2008b3572b329d38b37d
Billy Hernandez <hernanbi@ecs.csus.edu>
2020-05-25 23:45:27 -0700

    displays different icon when looking at shopping page. If company name is bluesign approved it shows in browseraction badge

--- Extension/background.js
+++ Extension/background.js
@@ -1,34 +1,58 @@
-// chrome.browserAction.disable()
+chrome.browserAction.setIcon({path : {"16":"icons/gray_icon16.png"}})
 
-// console.log("hi")
-// // var mySiteUrl = "https://www.google.com";
-// // chrome.tabs.getCurrent(function (tabs) {
-// //     console.log(tabs[0]);
-// //     console.log(tabs[0].url);
-// //     if(tabs[0].url === mySiteUrl){
-// //       chrome.browserAction.enable()
-// //     }
-// // });
-// chrome.tabs.onUpdated.addListener(checkURL());
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.shoppingPage == true) {
+      console.log("somehow true");
+      console.log(request.shoppingPage);
+      chrome.browserAction.setIcon({path : {"16": "icons/get_started16.png"}})
+
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab :
+                  "from the extension");
+      console.log(sender.tab.title.split(' ')[0])
+      var companyName = sender.tab.title.split(' ')[0];
+
+      var blueSignRequest = new XMLHttpRequest()
+      var url = 'http://localhost:8080/score/' + companyName;
+      blueSignRequest.open('GET', url, true)
+      blueSignRequest.onload = function() {
+        console.log(this.response);
+        if (this.response == "true") {
+          chrome.browserAction.setBadgeText({text:"Yay"});
+        }
+        else {
+          chrome.browserAction.setBadgeText({text:"Poo"});
+        }
+      }
+      blueSignRequest.send()
+    }
+    else {
+      console.log(request.shoppingPage);
+      chrome.browserAction.setIcon({path : {"16":"icons/gray_icon16.png"}})
+      chrome.browserAction.setBadgeText({text:""});
+    }
+    sendResponse({acknowledge: "okay"});
+});
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab :
+//                 "from the extension");
+//     console.log(sender.tab.title.split(' ')[0])
+//     var companyName = sender.tab.title.split(' ')[0];
 //
-// function checkURL() {
-//   chrome.tabs.query(
-//     {active:true,windowType:"normal", currentWindow: true},
-//     function(d){console.log(d);
+//     var blueSignRequest = new XMLHttpRequest()
+//     var url = 'http://localhost:8080/score/' + companyName;
+//     blueSignRequest.open('GET', url, true)
+//     blueSignRequest.onload = function() {
+//       console.log(this.response);
+//       if (this.response == "true") {
+//         // chrome.pageAction.setBadgeText({text:"Yay"});
+//       }
+//       else {
+//         // chrome.pageAction.setBadgeText({text:"Poo"});
+//       }
+//     }
+//     blueSignRequest.send()
 //   });
-// }
-var companyName = "adida";
-
-var request = new XMLHttpRequest()
-var url = 'http://localhost:8080/score/' + companyName;
-request.open('GET', url, true)
-request.onload = function() {
-  console.log(this.response);
-  if (this.response == "true") {
-    chrome.browserAction.setBadgeText({text:"Yay"});
-  }
-  else {
-    chrome.browserAction.setBadgeText({text:"Poo"});
-  }
-}
-request.send()
--- Extension/content.js
+++ Extension/content.js
@@ -1,4 +1,24 @@
-var word = "product";
-var queue = [document.body];
 var title = document.title;
 console.log(title);
+
+window.onload = function() {
+  var dom = document.getElementsByTagName('html')[0].innerHTML;
+  console.log(dom);
+  var present = dom.search("content=\"product\"")
+  if (present == -1) {
+    present = dom.search("gl-product-card-container")
+  }
+  console.log(present);
+  if (present != -1) {
+    console.log("found");
+    chrome.runtime.sendMessage({shoppingPage: true}, function(response) {
+      console.log(response.acknowledge);
+    });
+  }
+  else {
+    console.log("not found");
+    chrome.runtime.sendMessage({shoppingPage: false}, function(response) {
+      console.log(response.acknowledge);
+    });
+  }
+};
