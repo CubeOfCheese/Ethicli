e12b3b65b28f46edd588daec2e2b50db44162481
Linda <lindathehuang@gmail.com>
2020-05-29 13:23:29 -0700

    Added style change depending on webpage type
    Displays "not a shop page" message when the user isn't on a shop page. Cahnged some styling.

--- Extension/content.js
+++ Extension/content.js
@@ -1,14 +1,17 @@
-
 window.onload = function() {
-  var dom = document.getElementsByTagName('html')[0].innerHTML;
-  var present = dom.search("content=\"product\"")
-  if (present == -1) {
-    present = dom.search("gl-product-card-container")
-  }
-  if (present != -1) {
-    chrome.runtime.sendMessage({shoppingPage: true}, function(response) {});
-  }
-  else {
-    chrome.runtime.sendMessage({shoppingPage: false}, function(response) {});
-  }
+    var dom = document.getElementsByTagName('html')[0].innerHTML;
+    var present = dom.search("content=\"product\"")
+    if (present == -1) {
+        present = dom.search("gl-product-card-container")
+    }
+    if (present != -1) {
+        chrome.runtime.sendMessage({ shoppingPage: true }, function(response) {});
+        document.getElementById("shopping").classList.remove("notShop");
+        document.getElementById("notShopMessage").classList.remove("notShop");
+    } else {
+        chrome.runtime.sendMessage({ shoppingPage: false }, function(response) {});
+        document.getElementById("shopping").classList.add("notShop");
+        document.getElementById("notShopMessage").classList.add("notShop");
+        document.body.style.height = "150px";
+    }
 };
