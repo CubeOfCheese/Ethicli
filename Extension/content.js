3314e83c349d1e0c54e607c3d9f1bf72ccaab14d
Linda <lindathehuang@gmail.com>
2020-05-29 23:46:15 -0700

    Added array comparing for present words
    Parses array to compare words/prhases instead of multiple if-statements. Works on GAP and Adidas!

--- Extension/content.js
+++ Extension/content.js
@@ -1,12 +1,19 @@
 window.onload = function() {
     console.log("content.js");
     var dom = document.getElementsByTagName('html')[0].innerHTML;
-    var present = dom.search("content=\"product\"")
-    if (present == -1) {
-        present = dom.search("gl-product-card-container")
-    }
 
-    if (present != -1) {
+    var shopWords = ["content=\"product\"", "gl-product-card-container"];
+    var wordTracker = 0;
+    for (i = 0; i < shopWords.length; i++) {
+        var present = dom.search(shopWords[i]);
+        if (present > -1) { // if words are present, add values.
+            wordTracker += 1;
+        }
+        console.log("present: " + present.toString());
+    }
+    console.log("wordTracker: " + wordTracker.toString());
+
+    if (wordTracker > 0) { //if there's at least one shopWord present
         chrome.runtime.sendMessage({ shoppingPage: true }, function(response) {});
     } else {
         chrome.runtime.sendMessage({ shoppingPage: false }, function(response) {});
