977045d1120165b70615d24fbd9f6816cab5c366
CubeOfCheese <cubeofcheese@gmail.com>
2020-06-02 14:23:13 -0700

    Properly cuts off end of url for compatibility with Big Picture API

--- Extension/background.js
+++ Extension/background.js
@@ -44,7 +44,8 @@ chrome.runtime.onMessage.addListener(
 function getCompanyName(companyUrl) {
   if (companyUrl.substring(0, 8) == "https://") companyUrl = companyUrl.substring(8);
   else if (companyUrl.substring(0, 7) == "http://") companyUrl = companyUrl.substring(7);
-  if (companyUrl.substring(companyUrl.length-1) == "/") companyUrl = companyUrl.substring(0, companyUrl.length-1);
+  var endOfBaseDomain = companyUrl.search("/");
+  if (endOfBaseDomain > -1) companyUrl = companyUrl.substring(0, endOfBaseDomain);
   var fetchUrl = "https://company.bigpicture.io/v1/companies/find?domain=" + companyUrl;
   var fetchParams = {headers: {
                         'Authorization': env.COMPANY_NAME_API_KEY // hidden from github bots
