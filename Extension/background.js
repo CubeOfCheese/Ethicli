78ecab8320e61d300081f908b9278cf9c051b98d
Linda <lindathehuang@gmail.com>
2020-06-02 15:21:57 -0700

    Added link to JSON and fixed parentheses
    Added link to JSON and fixed parentheses. Also added .toLowerCase() for shop names

--- Extension/background.js
+++ Extension/background.js
@@ -23,15 +23,17 @@ chrome.runtime.onMessage.addListener(
                   } else {
                       chrome.browserAction.setBadgeText({ text: "Poo" });
                   }
+                  console.log(this.response);
+                  console.log(JSON.parse(this.response).bluesignPartner);
               }
               blueSignRequest.send()
             });
-
         } else {
             isShoppingPage = false;
             chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
             chrome.browserAction.setBadgeText({ text: "" });
         }
+      }
     }
 );
 
@@ -44,18 +46,20 @@ chrome.runtime.onMessage.addListener(
             sendResponse({ mainRate: mainRating });
         }
     }
-});
+);
+
 function getCompanyName(companyUrl) {
   if (companyUrl.substring(0, 8) == "https://") companyUrl = companyUrl.substring(8);
   else if (companyUrl.substring(0, 7) == "http://") companyUrl = companyUrl.substring(7);
   var endOfBaseDomain = companyUrl.search("/");
   if (endOfBaseDomain > -1) companyUrl = companyUrl.substring(0, endOfBaseDomain);
   var fetchUrl = "https://company.bigpicture.io/v1/companies/find?domain=" + companyUrl;
-  var fetchParams = {headers: {
-                        'Authorization': env.COMPANY_NAME_API_KEY // hidden from github bots
-                      }
-                    }
+  var fetchParams = {
+      headers: {
+        'Authorization': env.COMPANY_NAME_API_KEY // hidden from github bots
+      }
+  }
   return fetch(fetchUrl, fetchParams)
   .then(data=>{return data.json()})
-  .then(res=>{return res.name})
+  .then(res=>{return res.name.toLowerCase();})
 }
