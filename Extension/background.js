c83c51485a82e317300597c97ec9a80f7d306c05
Linda <38482675+lindaicing@users.noreply.github.com>
2020-06-02 13:08:41 -0700

    Added way to pass score from background.js to popup.js (#46)
    * Added way to pass score from background.js to popup.js
    
    Added hard-coded score that is passed from background.js to popup.js.
    
    Changed extention badge from displaying "yay/poo" to display the general rating using toString();
    
    Added change in extension badge color depending on score.
    
    Updated colors to match color scheme.
    
    * Added newlines to ends of code
    
    Added newlines to ends of code
    
    * Elongated Variable Name
    
    from mr to MainRate

--- Extension/background.js
+++ Extension/background.js
@@ -1,37 +1,48 @@
 chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
 
 var isShoppingPage;
+var mainRating = 5.0;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.msgName == "PageEvaluated") {
-        if (request.shoppingPage == true) {
-            chrome.browserAction.setIcon({ path: { "16": "icons/get_started16.png" } })
-            isShoppingPage = true;
-            var companyName = sender.tab.title.split(' ')[0];
-            var blueSignRequest = new XMLHttpRequest()
-            var url = 'http://ethicli.com/score/' + companyName;
-            blueSignRequest.open('GET', url, true)
-            blueSignRequest.onload = function() {
-                if (this.response == "true") {
-                    chrome.browserAction.setBadgeText({ text: "Yay" });
-                } else {
-                    chrome.browserAction.setBadgeText({ text: "Poo" });
+        if (request.msgName == "PageEvaluated") {
+            if (request.shoppingPage == true) {
+                chrome.browserAction.setIcon({ path: { "16": "icons/get_started16.png" } })
+                isShoppingPage = true;
+                var companyName = sender.tab.title.split(' ')[0];
+                var blueSignRequest = new XMLHttpRequest()
+                var url = 'http://ethicli.com/score/' + companyName;
+                blueSignRequest.open('GET', url, true)
+                blueSignRequest.onload = function() {
+                    if (this.response == "true") {
+                        mainRating = 10.0;
+                    } else {
+                        mainRating = 8.5;
+                    }
+                    chrome.browserAction.setBadgeText({ text: mainRating.toString() });
+                    if (mainRating >= 5) {
+                        chrome.browserAction.setBadgeBackgroundColor({ color: "#3b753f" });
+                    } else {
+                        chrome.browserAction.setBadgeBackgroundColor({ color: "#05668D" });
+                    }
                 }
+                blueSignRequest.send()
+            } else {
+                isShoppingPage = false;
+                chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
+                chrome.browserAction.setBadgeText({ text: "" });
             }
-            blueSignRequest.send()
-        } else {
-            isShoppingPage = false;
-            chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
-            chrome.browserAction.setBadgeText({ text: "" });
         }
-      }
     }
 );
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.msgName == "isShoppingPage?") {
-      sendResponse({shoppingPage: isShoppingPage});
+    function(request, sender, sendResponse) {
+        if (request.msgName == "isShoppingPage?") {
+            sendResponse({ shoppingPage: isShoppingPage });
+        }
+        if (request.msgName == "whatsMainRating?") {
+            sendResponse({ mainRate: mainRating });
+        }
     }
-});
+);
