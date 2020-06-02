8d30dcf703769e12ce6abadc115e47afe46c30d6
CubeOfCheese <cubeofcheese@gmail.com>
2020-06-02 14:37:42 -0700

    Company name recognition (#48)
    * utilizes Big Picture API to retrieve company name based on url. 
    
    * Properly cuts off end of url for compatibility with Big Picture API

--- Extension/background.js
+++ Extension/background.js
@@ -5,33 +5,32 @@ var mainRating = 5.0;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.msgName == "PageEvaluated") {
-            if (request.shoppingPage == true) {
-                chrome.browserAction.setIcon({ path: { "16": "icons/get_started16.png" } })
-                isShoppingPage = true;
+      if (request.msgName == "PageEvaluated") {
+        if (request.shoppingPage == true) {
+            chrome.browserAction.setIcon({ path: { "16": "icons/get_started16.png" } })
+            isShoppingPage = true;
+            var companyNamePromise = getCompanyName(sender.tab.url)
+            companyNamePromise.then(companyName=>{
+              if (companyName == null) {
                 var companyName = sender.tab.title.split(' ')[0];
-                var blueSignRequest = new XMLHttpRequest()
-                var url = 'http://ethicli.com/score/' + companyName;
-                blueSignRequest.open('GET', url, true)
-                blueSignRequest.onload = function() {
-                    if (this.response == "true") {
-                        mainRating = 10.0;
-                    } else {
-                        mainRating = 8.5;
-                    }
-                    chrome.browserAction.setBadgeText({ text: mainRating.toString() });
-                    if (mainRating >= 5) {
-                        chrome.browserAction.setBadgeBackgroundColor({ color: "#3b753f" });
-                    } else {
-                        chrome.browserAction.setBadgeBackgroundColor({ color: "#05668D" });
-                    }
-                }
-                blueSignRequest.send()
-            } else {
-                isShoppingPage = false;
-                chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
-                chrome.browserAction.setBadgeText({ text: "" });
-            }
+              }
+              var blueSignRequest = new XMLHttpRequest()
+              var url = 'http://ethicli.com/score/' + companyName;
+              blueSignRequest.open('GET', url, true)
+              blueSignRequest.onload = function() {
+                  if (this.response == "true") {
+                      chrome.browserAction.setBadgeText({ text: "Yay" });
+                  } else {
+                      chrome.browserAction.setBadgeText({ text: "Poo" });
+                  }
+              }
+              blueSignRequest.send()
+            });
+
+        } else {
+            isShoppingPage = false;
+            chrome.browserAction.setIcon({ path: { "16": "icons/gray_icon16.png" } })
+            chrome.browserAction.setBadgeText({ text: "" });
         }
     }
 );
@@ -45,4 +44,18 @@ chrome.runtime.onMessage.addListener(
             sendResponse({ mainRate: mainRating });
         }
     }
-);
+});
+function getCompanyName(companyUrl) {
+  if (companyUrl.substring(0, 8) == "https://") companyUrl = companyUrl.substring(8);
+  else if (companyUrl.substring(0, 7) == "http://") companyUrl = companyUrl.substring(7);
+  var endOfBaseDomain = companyUrl.search("/");
+  if (endOfBaseDomain > -1) companyUrl = companyUrl.substring(0, endOfBaseDomain);
+  var fetchUrl = "https://company.bigpicture.io/v1/companies/find?domain=" + companyUrl;
+  var fetchParams = {headers: {
+                        'Authorization': env.COMPANY_NAME_API_KEY // hidden from github bots
+                      }
+                    }
+  return fetch(fetchUrl, fetchParams)
+  .then(data=>{return data.json()})
+  .then(res=>{return res.name})
+}
