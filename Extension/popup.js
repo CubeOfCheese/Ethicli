df80c237ca86abac5047a1a5f53e2af30223819f
Linda <lindathehuang@gmail.com>
2020-06-01 23:54:13 -0700

    Added way to pass score from background.js to popup.js
    Added hard-coded score that is passed from background.js to popup.js.
    
    Changed extention badge from displaying "yay/poo" to display the general rating using toString();
    
    Added change in extension badge color depending on score.
    
    Updated colors to match color scheme.

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,11 +1,17 @@
 chrome.runtime.sendMessage({ msgName: "isShoppingPage?" }, function(response) {
-  if (response.shoppingPage == true) {
-    document.getElementById("shopping").classList.remove("notShop");
-    document.getElementById("notShopMessage").classList.remove("notShop");
-  }
-  else {
-    document.getElementById("shopping").classList.add("notShop");
-    document.getElementById("notShopMessage").classList.add("notShop");
-    document.body.style.height = "150px";
-  }
+    if (response.shoppingPage == true) {
+        document.getElementById("shopping").classList.remove("notShop");
+        document.getElementById("notShopMessage").classList.remove("notShop");
+    } else {
+        document.getElementById("shopping").classList.add("notShop");
+        document.getElementById("notShopMessage").classList.add("notShop");
+        document.body.style.height = "140px";
+    }
 });
+
+window.onload = function() {
+    chrome.runtime.sendMessage({ msgName: "whatsMainRating?" }, function(response) {
+        document.getElementById("overallScore").innerHTML = response.mr;
+        console.log(response.mr);
+    });
+}
