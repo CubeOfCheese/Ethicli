f558dad1f444e1729e900dc625c6e78d488f080f
CubeOfCheese <cubeofcheese@gmail.com>
2020-06-04 17:19:52 -0700

    Adds onclick function

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,10 +1,10 @@
 chrome.runtime.sendMessage({ msgName: "isShoppingPage?" }, function(response) {
     if (response.shoppingPage == true) {
-        document.getElementById("shopping").classList.remove("noShopData");
-        document.getElementById("notShopMessage").classList.remove("noShopData");
+        document.getElementById("shopping").classList.remove("notShop");
+        document.getElementById("notShopMessage").classList.remove("notShop");
     } else {
-        document.getElementById("shopping").classList.add("noShopData");
-        document.getElementById("notShopMessage").classList.add("noShopData");
+        document.getElementById("shopping").classList.add("notShop");
+        document.getElementById("notShopMessage").classList.add("notShop");
         document.body.style.height = "140px";
     }
 });
@@ -12,12 +12,12 @@ chrome.runtime.sendMessage({ msgName: "isShoppingPage?" }, function(response) {
 window.onload = function() {
     chrome.runtime.sendMessage({ msgName: "whatsMainRating?" }, function(response) {
         var ethicliScore = (response.ethicliStats.overallScore/20).toFixed(1);
+        console.log(ethicliScore);
         if(response.ethicliStats.bcorpCertified && response.ethicliStats.bluesignPartner){
             ethicliScore = (response.ethicliStats.overallScore/20).toFixed(1)+1;
         }else if(response.ethicliStats.bluesignPartner){
             ethicliScore = 7.5;
         }
-
         document.getElementById("overallScore").innerHTML = ethicliScore;
         if(response.ethicliStats.bcorpCertified){
             document.getElementById("bcorp").classList.add("trueForPage");
@@ -25,10 +25,28 @@ window.onload = function() {
         if(response.ethicliStats.bluesignPartner){
             document.getElementById("bluesign").classList.add("trueForPage");
         }
-        if((ethicliScore==0.0)||isNaN(ethicliScore)){
-            document.getElementById("shopping").classList.add("noShopData");
-            document.getElementById("shopDataUnavailable").classList.add("noShopData");
-            document.body.style.height = "140px";
-        }
     });
+}
+somethingWrong()
+function somethingWrong() {
+  var query = { active: true, currentWindow: true };
+  chrome.tabs.query(query, function callback(tabs) {
+    var currentTab = tabs[0];
+    console.log(currentTab.url);
+    var fetchUrl = "http://ethicli.com/feedback/";
+    var errorType;  // Needs to be implemented later. errorType will communicate
+                    // wether the problem is a shop not being identified or a shop having the wrong data
+    let fetchData = {
+      url: currentTab.url,
+      error: errorType
+    };
+    let fetchParams = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(fetchData)
+    }
+    fetch(fetchUrl, fetchParams)
+  });
 }
