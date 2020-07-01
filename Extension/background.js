b3c13b764de321c8ac5c17effb835320a0aa59c4
CubeOfCheese <cubeofcheese@gmail.com>
2020-06-30 23:48:09 -0700

    Fix rating (#149)
    Properly retrieves manual ratings and updates some variable names
    
    Co-authored-by: Linda <lindathehuang@gmail.com>

--- Extension/background.js
+++ Extension/background.js
@@ -24,20 +24,23 @@ function reloadExt(request, sender, sendResponse) {
                     }
                     companyName = companyName.split(' ')[0];
 
-                    var blueSignRequest = new XMLHttpRequest()
+                    var companyRequest = new XMLHttpRequest()
                     var url = 'http://ethicli.com/score/' + companyName;
-                    blueSignRequest.open('GET', url, true)
-                    blueSignRequest.onload = function() {
+                    companyRequest.open('GET', url, true)
+                    companyRequest.onload = function() {
                         var jsonResponse = JSON.parse(this.response);
                         ethicliStats = jsonResponse;
-
-                        ethicliBadgeScore = Math.round(jsonResponse.overallScore / 20);
-                        if (jsonResponse.bcorpCertified && jsonResponse.bluesignPartner) {
-                            ethicliBadgeScore += 1;
-                        } else if (jsonResponse.bluesignPartner) {
-                            ethicliBadgeScore = 7.5;
+                        var ethicliBadgeScore;
+                        if (jsonResponse.overallScore > 0) {
+                            ethicliBadgeScore = Math.round(jsonResponse.overallScore);
+                        } else {
+                            ethicliBadgeScore = Math.round(jsonResponse.bcorpScore / 20);
+                            if (jsonResponse.bcorpCertified && jsonResponse.bluesignPartner) {
+                                ethicliBadgeScore += 1;
+                            } else if (jsonResponse.bluesignPartner) {
+                                ethicliBadgeScore = 7.5;
+                            }
                         }
-
                         if ((isNaN(jsonResponse.overallScore)) || (ethicliBadgeScore == 0)) {
                             ethicliBadgeScore = "";
                             chrome.browserAction.setPopup({ popup: "popupNoRating.html", tabId: currentTab.id })
@@ -46,7 +49,7 @@ function reloadExt(request, sender, sendResponse) {
                         }
                         chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: currentTab.id });
                     }
-                    blueSignRequest.send()
+                    companyRequest.send()
                 });
             } else {
                 isShoppingPage = false;
--- Extension/popup.js
+++ Extension/popup.js
@@ -6,37 +6,41 @@ chrome.runtime.sendMessage({ msgName: "isShoppingPage?" }, function(response) {
 
 function loadExtension() {
     chrome.runtime.sendMessage({ msgName: "whatsMainRating?" }, function(response) {
-        var ethicliScore = (response.ethicliStats.overallScore / 20).toFixed(1);
-        if (response.ethicliStats.bcorpCertified && response.ethicliStats.bluesignPartner) {
-            // bluesign partners get an extra point added to their score
-            ethicliScore = (response.ethicliStats.overallScore / 20 + 1).toFixed(1);
-        } else if (!response.ethicliStats.bcorpCertified && response.ethicliStats.bluesignPartner) {
-            ethicliScore = 7.5;
+        var ethicliScore;
+        if(response.ethicliStats.overallScore>0){
+            ethicliScore = (response.ethicliStats.overallScore).toFixed(1)
+        }else{
+            ethicliScore = (response.ethicliStats.bcorpScore / 20).toFixed(1)
+            if (response.ethicliStats.bcorpCertified && response.ethicliStats.bluesignPartner) {
+                // bluesign partners get an extra point added to their score
+                ethicliScore = (response.ethicliStats.bcorpScore / 20 + 1).toFixed(1);
+            } else if (!response.ethicliStats.bcorpCertified && response.ethicliStats.bluesignPartner) {
+                ethicliScore = 7.5;
+            }
         }
+
         if(document.getElementById("overallScore")!== null){ //checks to see if ID even appears on page
             document.getElementById("overallScore").innerHTML = ethicliScore;
         }
 
-        var bt = 0;
-        console.log("BOB: "+response.ethicliStats.blackOwnedBusiness);
-        console.log("BLM: "+response.ethicliStats.supportsBLM);
+        var badgeCounter = 0;
         if (response.ethicliStats.bcorpCertified) {
             document.getElementById("bcorp").classList.add("trueForPage");
-            bt++;
+            badgeCounter++;
         }
         if (response.ethicliStats.bluesignPartner) {
             document.getElementById("bluesign").classList.add("trueForPage");
-            bt++;
+            badgeCounter++;
         }
         if (response.ethicliStats.blackOwnedBusiness) {
             document.getElementById("blackowned").classList.add("trueForPage");
-            bt++;
+            badgeCounter++;
         }
         if (response.ethicliStats.supportsBLM) {
             document.getElementById("blmsupport").classList.add("trueForPage");
-            bt++;
+            badgeCounter++;
         }
-        if(bt>0){
+        if(badgeCounter>0){
             document.getElementById("noBadge").style.display = "none";
             document.getElementById("hasBadge").style.display = "block";
             document.body.style = "height:190px;"
