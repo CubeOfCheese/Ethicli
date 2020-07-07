d6f9997f875cac514383f9ae7458dd092220b4ff
Linda <38482675+lindaicing@users.noreply.github.com>
2020-07-06 20:17:00 -0700

    Added privacy policy and updated links (#162)
    Added privacy policy and updated links
    Linked nav for about sections of each page to main index’s about section
    Added ID’s for each page
    Removed credits that only appear on main page
    Delete .DS_Store
    Removed links on privacy policy
    Added updates to Privacy Policy

--- Website-and-Backend/src/main/resources/public/stylescripts/script.js
+++ Website-and-Backend/src/main/resources/public/stylescripts/script.js
@@ -2,34 +2,34 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($(window).width() > 768) {
             if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2800)) {
-                $("#navlinks a").css({
+                $("#index #navlinks a").css({
                     "background": "#102340",
                     "color": "#FEFCF3"
                 });
-                $("#navlinks a").on("mouseover", function() {
+                $("#index #navlinks a").on("mouseover", function() {
                     $(this).css({
                         "background": "#2B6589",
                         "color": "#FFFCF2"
                     });
                 })
-                $("#navlinks a").on("mouseleave", function() {
+                $("#index #navlinks a").on("mouseleave", function() {
                     $(this).css({
                         "background": "#102340",
                         "color": "#FEFCF3"
                     });
                 })
             } else {
-                $("#navlinks a").css({
+                $("#index #navlinks a").css({
                     "background": "#C9D4C6",
                     "color": "#102340"
                 });
-                $("#navlinks a").on("mouseover", function() {
+                $("#index #navlinks a").on("mouseover", function() {
                     $(this).css({
                         "background": "#2B6589",
                         "color": "#FFFCF2"
                     });
                 })
-                $("#navlinks a").on("mouseleave", function() {
+                $("#index #navlinks a").on("mouseleave", function() {
                     $(this).css({
                         "background": "#C9D4C6",
                         "color": "#102340"
@@ -37,17 +37,17 @@ $(document).ready(function() {
                 })
             }
         } else {
-            $("#navlinks a").css({
+            $("#index #navlinks a").css({
                 "background": "#8EA289",
                 "color": "#FFFCF2"
             });
-            $("#navlinks a").on("mouseover", function() {
+            $("#index #navlinks a").on("mouseover", function() {
                 $(this).css({
                     "background": "#2B6589",
                     "color": "#FFFCF2"
                 });
             })
-            $("#navlinks a").on("mouseleave", function() {
+            $("#index #navlinks a").on("mouseleave", function() {
                 $(this).css({
                     "background": "#8EA289",
                     "color": "#FFFCF2"
