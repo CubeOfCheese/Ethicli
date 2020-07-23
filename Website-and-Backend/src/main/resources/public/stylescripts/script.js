9dda6d912fbcb4f46a001ce556631f909e0ef4e1
Linda <lindathehuang@gmail.com>
2020-07-23 10:44:46 -0700

    Fixed menu colors
    Put color changing into functions and adjusted for window widths

--- Website-and-Backend/src/main/resources/public/stylescripts/script.js
+++ Website-and-Backend/src/main/resources/public/stylescripts/script.js
@@ -1,40 +1,22 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-        if ($(window).width() > 768) {
-            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2220)) {
-                $("#index #navlinks a").css({
-                    "background": "#102340",
-                    "color": "#FEFCF3"
-                });
-                $("#index #navlinks a").on("mouseover", function() {
-                    $(this).css({
-                        "background": "#2B6589",
-                        "color": "#FFFCF2"
-                    });
-                })
-                $("#index #navlinks a").on("mouseleave", function() {
-                    $(this).css({
-                        "background": "#102340",
-                        "color": "#FEFCF3"
-                    });
-                })
+        if ($(window).width() > 768 && $(window).width() <= 800) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2300)) {
+                darkNav();
             } else {
-                $("#index #navlinks a").css({
-                    "background": "#C9D4C6",
-                    "color": "#102340"
-                });
-                $("#index #navlinks a").on("mouseover", function() {
-                    $(this).css({
-                        "background": "#2B6589",
-                        "color": "#FFFCF2"
-                    });
-                })
-                $("#index #navlinks a").on("mouseleave", function() {
-                    $(this).css({
-                        "background": "#C9D4C6",
-                        "color": "#102340"
-                    });
-                })
+                lightNav();
+            }
+        } else if ($(window).width() > 800 && $(window).width() <= 850) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2340)) {
+                darkNav();
+            } else {
+                lightNav();
+            }
+        } else if ($(window).width() > 850) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2400)) {
+                darkNav();
+            } else {
+                lightNav();
             }
         } else {
             $("#index #navlinks a").css({
@@ -51,6 +33,44 @@ $(document).ready(function() {
                 $(this).css({
                     "background": "#8EA289",
                     "color": "#FFFCF2"
+                });
+            })
+        }
+
+        function darkNav(){
+            $("#index #navlinks a").css({
+                "background": "#102340",
+                "color": "#FEFCF3"
+            });
+            $("#index #navlinks a").on("mouseover", function() {
+                $(this).css({
+                    "background": "#2B6589",
+                    "color": "#FFFCF2"
+                });
+            })
+            $("#index #navlinks a").on("mouseleave", function() {
+                $(this).css({
+                    "background": "#102340",
+                    "color": "#FEFCF3"
+                });
+            })
+        }
+
+        function lightNav(){
+            $("#index #navlinks a").css({
+                "background": "#C9D4C6",
+                "color": "#102340"
+            });
+            $("#index #navlinks a").on("mouseover", function() {
+                $(this).css({
+                    "background": "#2B6589",
+                    "color": "#FFFCF2"
+                });
+            })
+            $("#index #navlinks a").on("mouseleave", function() {
+                $(this).css({
+                    "background": "#C9D4C6",
+                    "color": "#102340"
                 });
             })
         }
