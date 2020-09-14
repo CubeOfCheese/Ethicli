7a7e240e8ded305f693bc35b3414b62b32bf4df7
Linda <lindathehuang@gmail.com>
2020-09-14 01:03:16 -0700

    Added tutorial menu to optin

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,41 +1,43 @@
 var hasSubscore;
+var newHeight = 360;
 
 chrome.runtime.sendMessage({ msgName: "isShoppingPage?" }, function(response) {
     if (response.shoppingPage) {
-      chrome.runtime.sendMessage({ msgName: "whatsMainRating?" }, function(response) {
-        loadExtension(response.ethicliStats);
-      });
+        chrome.runtime.sendMessage({ msgName: "whatsMainRating?" }, function(response) {
+            loadExtension(response.ethicliStats);
+        });
     }
 });
 
 function loadExtension(ethicliStats) {
     var ethicliScore = (ethicliStats.overallScore).toFixed(1);
-    if (ethicliScore > 0.0) {  // This will need to be updated once negative scores are used as default
-      adjustSubscores();
+    if (ethicliScore > 0.0) { // This will need to be updated once negative scores are used as default
+        adjustSubscores();
     }
+
     function adjustSubscores() {
         var fullheight = 360;
         if (ethicliStats.environmentScore == 0.0) {
-            fullheight = fullheight-50;
+            fullheight = fullheight - 50;
             document.getElementById("envSection").style = "display:none;";
         }
         if (ethicliStats.laborScore == 0.0) {
-            fullheight = fullheight-50;
+            fullheight = fullheight - 50;
             document.getElementById("laborSection").style = "display:none;";
         }
         if (ethicliStats.animalsScore == 0.0) {
-            fullheight = fullheight-50;
+            fullheight = fullheight - 50;
             document.getElementById("animalSection").style = "display:none;";
         }
         if (ethicliStats.socialScore == 0.0) {
-            fullheight = fullheight-50;
+            fullheight = fullheight - 50;
             document.getElementById("socialSection").style = "display:none;";
         }
         if (ethicliStats.environmentScore == 0.0 &&
             ethicliStats.laborScore == 0.0 &&
             ethicliStats.animalsScore == 0.0 &&
             ethicliStats.socialScore == 0.0
-        ){
+        ) {
             hasSubscore = false;
             document.getElementById("noSubscore").style = "display:block;";
             document.getElementById("detailsButton").style = "display:none;"
@@ -43,7 +45,7 @@ function loadExtension(ethicliStats) {
         } else {
             hasSubscore = true;
         }
-        var newHeight = "height:"+fullheight+"px;";
+        newHeight = "height:" + fullheight + "px;";
         document.body.style = newHeight;
     }
 
@@ -56,14 +58,14 @@ function loadExtension(ethicliStats) {
     //Change "View Details" button routing
     var query = { active: true, currentWindow: true };
     chrome.tabs.query(query, function callback(tabs) {
-      var currentTab = tabs[0];
-      var companyName = urlToCompanyName(currentTab.url);
+        var currentTab = tabs[0];
+        var companyName = urlToCompanyName(currentTab.url);
 
-      var infoLink = document.createElement("a");
-      infoLink.href = "https://ethicli.com/info/" + companyName;
-      infoLink.target = "_blank";
-      infoLink.textContent = "View Details";
-      document.getElementById("detailsButton").append(infoLink);
+        var infoLink = document.createElement("a");
+        infoLink.href = "https://ethicli.com/info/" + companyName;
+        infoLink.target = "_blank";
+        infoLink.textContent = "View Details";
+        document.getElementById("detailsButton").append(infoLink);
     })
 
     //Changes subratings
@@ -73,16 +75,16 @@ function loadExtension(ethicliStats) {
     document.getElementById("socialScore").textContent = ethicliStats.socialScore.toFixed(1);
 
     //Changes subratings scorebar
-    var envScore = ethicliStats.environmentScore*20;
+    var envScore = ethicliStats.environmentScore * 20;
     document.getElementById("envScoreBar").style.width = envScore + "px";
-    var laborScore = ethicliStats.laborScore*20;
+    var laborScore = ethicliStats.laborScore * 20;
     document.getElementById("laborScoreBar").style.width = laborScore + "px";
-    var animalScore = ethicliStats.animalsScore*20;
+    var animalScore = ethicliStats.animalsScore * 20;
     document.getElementById("animalScoreBar").style.width = animalScore + "px";
-    var socialScore = ethicliStats.socialScore*20;
+    var socialScore = ethicliStats.socialScore * 20;
     document.getElementById("socialScoreBar").style.width = socialScore + "px";
 
-    if (document.getElementById("overallScore")!== null) { //checks to see if ID even appears on page
+    if (document.getElementById("overallScore") !== null) { //checks to see if ID even appears on page
         document.getElementById("overallScore").textContent = ethicliScore;
     }
 
@@ -112,7 +114,7 @@ function loadExtension(ethicliStats) {
         document.getElementById("leapingbunny").classList.add("trueForPage");
         badgeCounter++;
     }
-    if (badgeCounter>0) {
+    if (badgeCounter > 0) {
         document.getElementById("noBadge").style.display = "none";
         document.getElementById("hasBadge").style.display = "block";
         document.body.style = "height:190px;"
@@ -133,63 +135,121 @@ window.onload = function() {
         somethingWrong();
     });
 
-    if(document.getElementById("scores") != null){ //if there is a scores ID present
+    if (document.getElementById("scores") != null) { //if there is a scores ID present
         document.getElementById("scores").onmouseover = function() {
             if (hasSubscore) {
-                document.getElementById("subscoreTip").style.left = (event.clientX-30)+"px";
-                document.getElementById("subscoreTip").style.top = (event.clientY-30)+"px";
+                document.getElementById("subscoreTip").style.left = (event.clientX - 30) + "px";
+                document.getElementById("subscoreTip").style.top = (event.clientY - 30) + "px";
             } else {
                 document.getElementById("subscoreTip").style = "display:none;";
             }
         };
     }
 
-    if(document.getElementById("sitename") != null){
+    if (document.getElementById("sitename") != null) {
         fadeLongURL();
+    }
+
+    //--- Tutorial --------------------------------------------------------------------------
+    var startTutorial = false;
+    var currentTutorial;
+    // document.getElementById("hideTutorial").onclick(function() {
+    //     $("#tutorial").toggleClass("hideTutorial");
+    //     if (startTutorial) {
+    //         $("#tutorial").removeClass("tutorialHeightAdjust");
+    //     } else {
+    //         $("tutorial").addClass("tutorialHeightAdjust");
+    //     }
+    // })
+
+    document.getElementById("watchTutorial").addEventListener("click", function() {
+        document.body.style = "height: 600px;";
+        startTutorial = !startTutorial
+        currentTutorial = 1;
+        if (startTutorial) {
+            document.getElementById("tutorial").style = "display: block";
+            document.getElementById("tutorialNavigation").style = "display:flex";
+            document.getElementById("tutorial" + currentTutorial).style = "display:block";
+        } else {
+            document.getElementById("tutorialScreens").style = "display:none";
+            document.getElementById("tutorialNavigation").style = "display:none";
+        }
+    })
+
+    var idname = "tutorial1";
+    document.getElementById("tutorialBack").addEventListener("click", function() {
+        currentTutorial -= 1;
+        tutorialSlideshow();
+    })
+    document.getElementById("tutorialNext").addEventListener("click", function() {
+        currentTutorial += 1;
+        tutorialSlideshow();
+    })
+
+    function tutorialSlideshow() {
+        idname = "tutorial" + (currentTutorial)
+        for (i = 0; i < 7; i++) {
+            var tutorialCycle = "tutorial" + (i + 1)
+            document.getElementById(tutorialCycle).style = "display:none";
+        }
+        if (currentTutorial < 1 || currentTutorial > 7) { // Resets tutorial
+            startTutorial = false;
+            currentTutorial = 1;
+            document.getElementById("tutorial").style = "display:none";
+            document.getElementById("tutorialNavigation").style = "display:none";
+            document.body.style = "height: " + newHeight + "px;";
+        } else {
+            document.getElementById(idname).style = "display:block";
+        }
+
+        if (currentTutorial == 6 || currentTutorial == 7) {
+            document.getElementById("tutorialNavigation").style = "display:flex; bottom:476px;";
+            console.log(document.getElementById("tutorialNavigation").style)
+        } else {
+            document.getElementById("tutorialNavigation").style = "display:flex; bottom:8px;";
+            console.log(document.getElementById("tutorialNavigation").style)
+        }
     }
 }
 
-function fadeLongURL(){
-    document.getElementById("siteurl").addEventListener("mouseover", function( event ) {
-        var siteurlLength = this.innerHTML.length+16;
-        if(siteurlLength > 30){
-            this.style = "margin-left: -"+(siteurlLength)+"px;";
-            document.getElementById("siteurlcontainer").style = 
+function fadeLongURL() {
+    document.getElementById("siteurl").addEventListener("mouseover", function(event) {
+        var siteurlLength = this.innerHTML.length + 16;
+        if (siteurlLength > 30) {
+            this.style = "margin-left: -" + (siteurlLength) + "px;";
+            document.getElementById("siteurlcontainer").style =
                 "-webkit-mask-image: linear-gradient(to right, transparent 0%, black 5%, black 100%, transparent 100%);\
                 mask-image: linear-gradient(to right, transparent 0%, black 5%, black 100%, transparent 100%)";
         }
     })
-    document.getElementById("siteurl").addEventListener("mouseout", function( event ) {
+    document.getElementById("siteurl").addEventListener("mouseout", function(event) {
         this.style = "margin-left: 0px;";
-        document.getElementById("siteurlcontainer").style = 
+        document.getElementById("siteurlcontainer").style =
             "-webkit-mask-image: linear-gradient(to right, black 90%, transparent 100%);\
             mask-image: linear-gradient(to right, black 90%, transparent 100%)";
     })
 }
 
 function urlToCompanyName(url) {
-  if (url.substring(0, 8) == "https://") {
-    url = url.substring(8);
-  }
-  else if (url.substring(0, 7) == "http://") {
-    url = url.substring(7);
-  }
+    if (url.substring(0, 8) == "https://") {
+        url = url.substring(8);
+    } else if (url.substring(0, 7) == "http://") {
+        url = url.substring(7);
+    }
 
-  if (url.substring(0, 4) == "www.") {
-    url = url.substring(4);
-  }
-  else if (url.substring(0, 4) == "us.") {
-    url = url.substring(3);
-  }
-  else if (url.substring(0, 4) == "docs.") {
-    url = url.substring(5);
-  }
+    if (url.substring(0, 4) == "www.") {
+        url = url.substring(4);
+    } else if (url.substring(0, 4) == "us.") {
+        url = url.substring(3);
+    } else if (url.substring(0, 4) == "docs.") {
+        url = url.substring(5);
+    }
 
-  var endOfBaseDomain = url.search(/\./);
-  if (endOfBaseDomain > -1) {
-    url = url.substring(0, endOfBaseDomain);
-  }
-  return url;
+    var endOfBaseDomain = url.search(/\./);
+    if (endOfBaseDomain > -1) {
+        url = url.substring(0, endOfBaseDomain);
+    }
+    return url;
 }
 
 function somethingWrong() {
@@ -211,8 +271,9 @@ function somethingWrong() {
 
         //Pulls and sets email
         document.getElementById("sendEmail").href = sendEmail();
+
         function sendEmail() {
-            var emailUrl = "mailto:hello@ethicli.com?subject=Error%20With%20Current%20Website%20&body=Error%20with%20the%20following%20page:%20"+currentTab.url+"%0d%0aPlease%20let%20us%20know%20what%20is%20wrong%20below.";
+            var emailUrl = "mailto:hello@ethicli.com?subject=Error%20With%20Current%20Website%20&body=Error%20with%20the%20following%20page:%20" + currentTab.url + "%0d%0aPlease%20let%20us%20know%20what%20is%20wrong%20below.";
             chrome.tabs.create({ url: emailUrl }, function(tab) {
                 setTimeout(function() {
                     chrome.tabs.remove(tab.id);
