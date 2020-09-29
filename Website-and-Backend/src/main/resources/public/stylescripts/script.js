65ee318bb08f90886dd80282fe2a692006687f87
Linda <38482675+lindaicing@users.noreply.github.com>
2020-09-28 20:01:06 -0700

    Website and faq update (#333)
    * Updated FAQ
    
    * Added partners section
    
    * fixed nav color change
    
    * Updated wording and affiliate link

--- Website-and-Backend/src/main/resources/public/stylescripts/script.js
+++ Website-and-Backend/src/main/resources/public/stylescripts/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if ($(window).width() > 768 && $(window).width() <= 800) {
-            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2870)) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 3940)) {
                 darkNav();
             } else {
                 lightNav();
             }
         } else if ($(window).width() > 800 && $(window).width() <= 850) {
-            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2900)) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 3960)) {
                 darkNav();
             } else {
                 lightNav();
             }
         } else if ($(window).width() > 850) {
-            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 2960)) {
+            if (($(this).scrollTop() < 730) || ($(this).scrollTop() > 4100)) {
                 darkNav();
             } else {
                 lightNav();
