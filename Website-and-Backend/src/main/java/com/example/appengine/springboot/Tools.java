328b2c913e0d6d31243fe0ea1a399fa3d9390b5f
Cosmo Stevens <cosmostevens@MacBook-Pro.local>
2020-10-13 17:19:02 -0700

    google format

--- Website-and-Backend/src/main/java/com/example/appengine/springboot/Tools.java
+++ Website-and-Backend/src/main/java/com/example/appengine/springboot/Tools.java
@@ -11,101 +11,96 @@ import java.nio.charset.StandardCharsets;
 
 public class Tools {
 
-    // Removes all occurences of char target from String name
-    public static String charRemove(String name, char target) {
-        char tempArray [] = name.toCharArray();
-        String output = "";
-        for (int a = 0;  a < name.length(); ++a) {
-            if (tempArray[a] != target)
-                output += tempArray[a];
-        }
-        return output;
+  // Removes all occurences of char target from String name
+  public static String charRemove(String name, char target) {
+    char tempArray[] = name.toCharArray();
+    String output = "";
+    for (int a = 0; a < name.length(); ++a) {
+      if (tempArray[a] != target) output += tempArray[a];
     }
+    return output;
+  }
 
-    // Compares searchTerm with business.website if it exists, if no match is found, it compares with business.name
-    public static boolean compare(String searchTerm, Business business) {
-        searchTerm = searchTerm.toLowerCase();
-        if (business.getWebsite() != null) {  // Compares searchTerm with business.website
-            String website = business.getWebsite().toLowerCase();
-            if (website.contains("http://")) {
-                website = website.substring(7, website.length());
-            } else if (website.contains("https://")) {
-                website = website.substring(8, website.length());
-            }
-            if (website.contains("www.")) {
-                website = website.substring(4, website.length());
-            }
-            if (website.length() > searchTerm.length()) {
-                if (searchTerm.equals(website.substring(0, searchTerm.length()))
-                        && website.charAt(searchTerm.length()) == '.') {
-                    return true;
-                }
-            }
+  // Compares searchTerm with business.website if it exists, if no match is found, it compares with
+  // business.name
+  public static boolean compare(String searchTerm, Business business) {
+    searchTerm = searchTerm.toLowerCase();
+    if (business.getWebsite() != null) { // Compares searchTerm with business.website
+      String website = business.getWebsite().toLowerCase();
+      if (website.contains("http://")) {
+        website = website.substring(7, website.length());
+      } else if (website.contains("https://")) {
+        website = website.substring(8, website.length());
+      }
+      if (website.contains("www.")) {
+        website = website.substring(4, website.length());
+      }
+      if (website.length() > searchTerm.length()) {
+        if (searchTerm.equals(website.substring(0, searchTerm.length()))
+            && website.charAt(searchTerm.length()) == '.') {
+          return true;
         }
-        // Compare searchTerm with business.name - Checks matches of words in business.name individually and together
-        String nameArray[] = prepareSearchTerm(business.getName().toLowerCase()).split(" ");
-        String nameToken;
-        for (int a = 0; a < nameArray.length; ++a) {
-            nameToken ="";
-            for (int b = a; b < nameArray.length; ++b) {
-                nameToken += nameArray[b];
-                if (searchTerm.equals(nameToken))
-                    return true;
-            }
-        }
-        return false;
+      }
     }
+    // Compare searchTerm with business.name - Checks matches of words in business.name individually
+    // and together
+    String nameArray[] = prepareSearchTerm(business.getName().toLowerCase()).split(" ");
+    String nameToken;
+    for (int a = 0; a < nameArray.length; ++a) {
+      nameToken = "";
+      for (int b = a; b < nameArray.length; ++b) {
+        nameToken += nameArray[b];
+        if (searchTerm.equals(nameToken)) return true;
+      }
+    }
+    return false;
+  }
 
-    public static String[] csvToStringArray(String content) {
-        String[] splitByComma;
-        String output = "";
-        boolean quoteDetected = false;
-        content = content.replaceAll("\"\"", "@@DOUBLEQUOTES@@");
-        splitByComma = content.split(",");
-        for (int a = 0; a < splitByComma.length; ++a) {
-            if (splitByComma[a].contains("\""))
-                quoteDetected = !quoteDetected;
-            if (quoteDetected) {
-                output += splitByComma[a] + ",";
-            } else {
-                output += splitByComma[a] + "@@DELIMITER@@";
-            }
-        }
-        return output
-                .replaceAll("\"", "")
-                .replaceAll("@@DOUBLEQUOTES@@", "\"")
-                .split("@@DELIMITER@@");
+  public static String[] csvToStringArray(String content) {
+    String[] splitByComma;
+    String output = "";
+    boolean quoteDetected = false;
+    content = content.replaceAll("\"\"", "@@DOUBLEQUOTES@@");
+    splitByComma = content.split(",");
+    for (int a = 0; a < splitByComma.length; ++a) {
+      if (splitByComma[a].contains("\"")) quoteDetected = !quoteDetected;
+      if (quoteDetected) {
+        output += splitByComma[a] + ",";
+      } else {
+        output += splitByComma[a] + "@@DELIMITER@@";
+      }
     }
+    return output.replaceAll("\"", "").replaceAll("@@DOUBLEQUOTES@@", "\"").split("@@DELIMITER@@");
+  }
 
-    // Removes unnecessary characters from searchTerm
-    public static String prepareSearchTerm(String searchTerm) {
-        searchTerm = searchTerm.toLowerCase();
-        searchTerm = charRemove(searchTerm, ',');
-        searchTerm = charRemove(searchTerm, '\'');
-        searchTerm = charRemove(searchTerm, ':');
-        searchTerm = charRemove(searchTerm, ';');
-        searchTerm = charRemove(searchTerm, '.');
-        searchTerm = charRemove(searchTerm, '-');
-        searchTerm = charRemove(searchTerm, '_');
-        searchTerm = charRemove(searchTerm, '&');
-        return searchTerm;
-    }
+  // Removes unnecessary characters from searchTerm
+  public static String prepareSearchTerm(String searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+    searchTerm = charRemove(searchTerm, ',');
+    searchTerm = charRemove(searchTerm, '\'');
+    searchTerm = charRemove(searchTerm, ':');
+    searchTerm = charRemove(searchTerm, ';');
+    searchTerm = charRemove(searchTerm, '.');
+    searchTerm = charRemove(searchTerm, '-');
+    searchTerm = charRemove(searchTerm, '_');
+    searchTerm = charRemove(searchTerm, '&');
+    return searchTerm;
+  }
 
-    // Compares companyName to Blocked URLS - Sheet.csv
-    public static boolean validateURL(String companyName) throws IOException {
-        Resource resource = new ClassPathResource("Blocked URLS - Sheet1.csv");
-        InputStream file = resource.getInputStream();
-        BufferedReader br = null;
-        String line = "";
-        try {
-            br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
-            while ((line = br.readLine()) != null) { // cycles through line by line
-                if (companyName.equals(line))
-                    return false;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
+  // Compares companyName to Blocked URLS - Sheet.csv
+  public static boolean validateURL(String companyName) throws IOException {
+    Resource resource = new ClassPathResource("Blocked URLS - Sheet1.csv");
+    InputStream file = resource.getInputStream();
+    BufferedReader br = null;
+    String line = "";
+    try {
+      br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
+      while ((line = br.readLine()) != null) { // cycles through line by line
+        if (companyName.equals(line)) return false;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return true;
+  }
 }
