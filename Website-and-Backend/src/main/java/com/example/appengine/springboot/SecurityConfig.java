feaf55287c3c3fb469527470604db1be1fa30c36
Cosmo Stevens <cosmostevens@MacBook-Pro.local>
2020-10-21 10:59:46 -0700

    working spring security with anonymous, user, and admin roles

--- Website-and-Backend/src/main/java/com/example/appengine/springboot/SecurityConfig.java
+++ Website-and-Backend/src/main/java/com/example/appengine/springboot/SecurityConfig.java
@@ -4,93 +4,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   // Authentication : User --> Roles
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth.inMemoryAuthentication().withUser("<>").password("{noop}<>")
-        .roles("USER").and().withUser("<>").password("{noop}<>")
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("<user-username>")
+        .password("{noop}<user-password>")
+        .roles("USER")
+        .and()
+        .withUser("<admin-username>")
+        .password("{noop}<admin-password>")
         .roles("USER", "ADMIN");
-
   }
 
   // Authorization : Role -> Access
-//  @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
-        .authorizeRequests()
-        .antMatchers("/").permitAll();
-//    ;
-    http
-//        .and()
-        .httpBasic()
+    http.httpBasic()
         .and()
         .authorizeRequests()
-        .antMatchers("/Advertisement/getByProductTags", "/Advertisement/getById", "/Advertisement/tagRegex", "/score/**").hasRole("USER")
-        .antMatchers("/Advertisement/getAll").hasRole("ADMIN")
-        .and().csrf().disable().headers().frameOptions().disable();
-
-
-
-
-
-
-
-//    http.authorizeRequests()
-//        .antMatchers("/faq");
-        // …are accessible to all users (authenticated or not).
-//        .permitAll();
-        //These paths…
-//        .antMatchers("/**")
-//        .hasRole("ADMIN")
-//        .antMatchers("/Advertisement/getByProductTags", "/Advertisement/getById", "/Advertisement/tagRegex", "/score/**")
-//    .hasRole("USER");
-
-
-//    http
-//        .anonymous().principal("guest").authorities("GUEST_ROLE")
-//        .and()
-//        .authorizeRequests()
-//.antMatchers("/score/**").permitAll()
-////        .antMatchers("/**").hasRole("ADMIN")
-//.antMatchers("/Advertisement/getAll").hasRole("USER")
-////    .antMatchers("scor")
-//        .and().formLogin()
-//        .loginPage("/login1")
-//        .successForwardUrl("/shared/poo!").failureForwardUrl("/shared/pee")
-//        .and().logout().logoutSuccessUrl("/mylogout").permitAll()
-//    .and().csrf().disable()
-//        ;
-
-//    http.authorizeRequests()
-//        .antMatchers("/faq")
-//     // …are accessible to all users (authenticated or not).
-//     .permitAll()
-//     //These paths…
-//     .antMatchers("/**")
-//     // …are only available to users with ADMIN role.
-//     .hasRole("ADMIN")
-//     // All remaining paths…
-//     .anyRequest()
-//     // ...require user to at least be authenticated
-//     .authenticated()
-//     .and()
-//     // If user isn't authorised to access a path...
-//     .exceptionHandling()
-//     // ...redirect them to /403
-//     .accessDeniedPage("/403");
-
-
-
-     //    http
-//        .authorizeRequests()
-//        .antMatchers("/**").authenticated()
-//        .antMatchers("/Advertisement/getAll").permitAll();
+        .antMatchers(
+            "/Advertisement/getByProductTags",
+            "/Advertisement/getById",
+            "/Advertisement/tagRegex",
+            "/score/**")
+        .hasRole("USER")
+        .antMatchers(
+            "/Advertisement/getAll",
+            "/Advertisement/add",
+            "/Advertisement/addAll",
+            "/Advertisement/update",
+            "/Advertisement/delete")
+        .hasRole("ADMIN")
+        .and()
+        .csrf()
+        .disable()
+        .headers()
+        .frameOptions()
+        .disable();
   }
-
 }
-
-
