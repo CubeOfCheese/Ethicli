d34200da8afd6241335ce37ddd3a4cad7d3c6460
Billy T. Hernandez <cubeofcheese@gmail.com>
2020-10-20 22:32:44 -0700

    Frontend code styling (#352)
    * All the automatic changes from eslint
    
    * triple equals, undeclared variables (no let used), and multiline strings
    
    * For ethicli urls, replaces all instance of http:// with https and removes www.
    
    * arrow function all the things!
    
    * Consolidated message passing receivers into switch statements
    
    * Uses fetch instead of xmlhttprequest in reloadExt()
    Implements some early returns (to reduce nesting)

--- Extension/background.js
+++ Extension/background.js
@@ -1,130 +1,28 @@
-chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" } })
+chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" } });
 
-var isShoppingPage;
-var ethicliStats;
-var productName;
+let isShoppingPage;
+let ethicliStats;
+let productName;
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        reloadExt(request, sender, sendResponse)
-    }
-);
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.msgName == "displayOptin") {
-        chrome.browserAction.setPopup({ popup: "popupOptin.html" })
-        chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }})
-        chrome.browserAction.setBadgeText({ text: "" });
-      }
-    }
-);
-
-function reloadExt(request, sender, sendResponse) {
-    if (request.msgName == "PageEvaluated") {
-        var query = { active: true, currentWindow: true };
-        chrome.tabs.query(query, function callback(tabs) {
-            var currentTab = tabs[0];
-
-            if (request.shoppingPage == true) {
-                chrome.browserAction.setIcon({ path: { "16": "icons/ethicli-16.png" }, tabId: currentTab.id })
-                isShoppingPage = true;
-
-                var companyName = urlToCompanyName(sender.tab.url);
-
-                var blacklist = ["google", "bing", "yahoo",  "baidu", "aol", "duckduckgo", "yandex", "ecosia"];
-                var notBlacklisted;
-                var ethicliBadgeScore;
-                for (b=0; b<blacklist.length; b++) {
-                    if (companyName.includes(blacklist[b])) {
-                        ethicliBadgeScore = "";
-                        notBlacklisted = false;
-                        break;
-                    }
-                    else {
-                        notBlacklisted = true;
-                    }
-                }
-
-                if (notBlacklisted) {
-                    var companyRequest = new XMLHttpRequest()
-                    var url = 'https://ethicli.com/score/' + companyName;
-                    companyRequest.open('GET', url, true)
-                    companyRequest.onload = function() {
-                        var jsonResponse = JSON.parse(this.response);
-                        ethicliStats = jsonResponse;
-                        ethicliBadgeScore = Math.round(jsonResponse.overallScore);
-
-                        if ((isNaN(jsonResponse.overallScore)) || (ethicliBadgeScore == 0)) {
-                            ethicliBadgeScore = "";
-                            chrome.browserAction.setPopup({ popup: "popupNoRating.html", tabId: currentTab.id })
-                            reportGA("Background-NoRating");
-                        } else {
-                            chrome.browserAction.setPopup({ popup: "popup.html", tabId: currentTab.id })
-                            reportGA("Background-HasRating");
-                        }
-                        chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: currentTab.id });
-                    }
-                    companyRequest.send();
-                }
-                else {
-                  notShop();
-                }
-            }
-            else {
-              notShop();
-            }
-
-            function notShop(){
-              isShoppingPage = false;
-              chrome.browserAction.setPopup({ popup: "popupNotShop.html", tabId: currentTab.id })
-              chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id })
-              chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
-            }
-          })
-    }
-    return true;
+function notShop(currentTab) {
+  isShoppingPage = false;
+  chrome.browserAction.setPopup({ popup: "popupNotShop.html", tabId: currentTab.id });
+  chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id });
+  chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
 }
 
-chrome.tabs.onActivated.addListener(
-    function() {
-        var query = { active: true, currentWindow: true };
-        chrome.tabs.query(query, function callback(tabs) {
-            var currentTab = tabs[0];
-            chrome.tabs.sendMessage(tabs[0].id, { msgName: "isShoppingPage?" }, function(response) {
-              // On first page visit, response is null. This function is only supposed to run after the first visit anyway,
-              // so this just gets rid of an error that didn't actually break anything
-              if (response) {
-                isShoppingPage = response.isShoppingPage;
-                // currentTab.url is null for new tab pages when first opened and equal to "chrome://newtab/" when navigated to from another tab.
-                if (isShoppingPage && currentTab.url && (currentTab.url != "chrome://newtab/")) {
-                // data should only be retrieved for actual pages that are shopping pages
-                    chrome.tabs.sendMessage(tabs[0].id, { msgName: "productIdentified?" }, function(response) {
-                        productName = response.productName
-                    })
-                    var request = { msgName: "PageEvaluated", shoppingPage: true };
-                    var sender = { tab: { url: "" } };
-                    sender.tab.url = currentTab.url;
-                    reloadExt(request, sender);
-                }
-              }
-            });
-        })
-    }
-);
-
 function urlToCompanyName(url) {
-  if (url.substring(0, 8) == "https://") {
+  if (url.substring(0, 8) === "https://") {
     url = url.substring(8);
-  }
-  else if (url.substring(0, 7) == "http://") {
+  } else if (url.substring(0, 7) === "http://") {
     url = url.substring(7);
   }
-  var endOfBaseDomain = url.search(/\//);
+  let endOfBaseDomain = url.search(/\//);
   if (endOfBaseDomain > -1) {
     url = url.substring(0, endOfBaseDomain);
   }
-  var endOfSubDomain = url.lastIndexOf('.', url.lastIndexOf('.')-1)
-  url = url.substring(endOfSubDomain+1);
+  const endOfSubDomain = url.lastIndexOf(".", url.lastIndexOf(".") - 1);
+  url = url.substring(endOfSubDomain + 1);
   endOfBaseDomain = url.search(/\./);
   if (endOfBaseDomain > -1) {
     url = url.substring(0, endOfBaseDomain);
@@ -132,74 +30,145 @@ function urlToCompanyName(url) {
   return url;
 }
 
-chrome.tabs.onCreated.addListener(
-    function() {
-        var query = { active: true, currentWindow: true };
-        chrome.tabs.query(query, function callback(tabs) {
-            var currentTab = tabs[0];
-            chrome.browserAction.setPopup({ popup: "popupNotShop.html", tabId: currentTab.id })
-            chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id })
-            chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
-        })
+function reloadExt(request, sender) {
+  const query = { active: true, currentWindow: true };
+  chrome.tabs.query(query, (tabs) => {
+    const currentTab = tabs[0];
+
+    if (!request.shoppingPage) {
+      notShop(currentTab);
     }
-)
-chrome.tabs.onUpdated.addListener(
-  function() {
-    var query = { active: true, currentWindow: true };
-    chrome.tabs.query(query, function callback(tabs) {
-        var currentTab = tabs[0];
-        chrome.tabs.sendMessage(currentTab.id, { msgName: "reevaluatePage" });
-    })
+    chrome.browserAction.setIcon({ path: { "16": "icons/ethicli-16.png" }, tabId: currentTab.id });
+    isShoppingPage = true;
+
+    const companyName = urlToCompanyName(sender.tab.url);
+
+    const blocklist = [ "google", "bing", "yahoo", "baidu", "aol", "duckduckgo", "yandex", "ecosia" ];
+    let isBlocklisted;
+    let ethicliBadgeScore;
+    for (let b = 0; b < blocklist.length; b++) {
+      if (companyName.includes(blocklist[b])) {
+        ethicliBadgeScore = "";
+        isBlocklisted = true;
+        break;
+      } else {
+        isBlocklisted = false;
+      }
+    }
+
+    if (isBlocklisted) {
+      notShop(currentTab);
+      return true;
+    }
+    const url = "https://ethicli.com/score/" + companyName;
+    return fetch(url).then((response) => response.json()).then((jsonResponse) => {
+      ethicliStats = jsonResponse;
+      ethicliBadgeScore = Math.round(jsonResponse.overallScore);
+
+      if ((isNaN(jsonResponse.overallScore)) || (ethicliBadgeScore === 0)) {
+        ethicliBadgeScore = "";
+        chrome.browserAction.setPopup({ popup: "popupNoRating.html", tabId: currentTab.id });
+        reportGA("Background-NoRating");
+      } else {
+        chrome.browserAction.setPopup({ popup: "popup.html", tabId: currentTab.id });
+        reportGA("Background-HasRating");
+      }
+      chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: currentTab.id });
+    });
+  });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.msgName) {
+    case "isShoppingPage?":
+      sendResponse({ shoppingPage: isShoppingPage });
+      break;
+    case "whatsMainRating?":
+      sendResponse({ ethicliStats: ethicliStats });
+      break;
+    case "productIdentified?":
+      sendResponse({ productName: productName });
+      break;
+    case "ProductIdentified":
+      productName = request.productName;
+      break;
+    case "PageEvaluated":
+      reloadExt(request, sender);
+      break;
+    case "displayOptin":
+      chrome.browserAction.setPopup({ popup: "popupOptin.html" });
+      chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" } });
+      chrome.browserAction.setBadgeText({ text: "" });
+      break;
+    default:
+      console.error(request, sender);
   }
-)
+});
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msgName == "ProductIdentified") {
-            productName = request.productName;
+chrome.tabs.onActivated.addListener(() => {
+  const query = { active: true, currentWindow: true };
+  chrome.tabs.query(query, (tabs) => {
+    const currentTab = tabs[0];
+    chrome.tabs.sendMessage(tabs[0].id, { msgName: "isShoppingPage?" }, (response) => {
+      // On first page visit, response is null. This function is only supposed to run after the first visit anyway,
+      // so this just gets rid of an error that didn't actually break anything
+      if (response) {
+        isShoppingPage = response.isShoppingPage;
+        // currentTab.url is null for new tab pages when first opened
+        // and equal to "chrome://newtab/" when navigated to from another tab.
+        if (isShoppingPage && currentTab.url && (currentTab.url !== "chrome://newtab/")) {
+          // data should only be retrieved for actual pages that are shopping pages
+          chrome.tabs.sendMessage(tabs[0].id, { msgName: "productIdentified?" }, (response) => {
+            productName = response.productName;
+          });
+          const request = { msgName: "PageEvaluated", shoppingPage: true };
+          const sender = { tab: { url: "" } };
+          sender.tab.url = currentTab.url;
+          reloadExt(request, sender);
         }
-    }
-);
+      }
+    });
+  });
+});
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msgName == "isShoppingPage?") {
-            sendResponse({ shoppingPage: isShoppingPage });
-        }
-        if (request.msgName == "whatsMainRating?") {
-            sendResponse({ ethicliStats: ethicliStats });
-        }
-        if (request.msgName == "productIdentified?") {
-            sendResponse({ productName: productName });
-        }
-    }
-);
+chrome.tabs.onCreated.addListener(() => {
+  const query = { active: true, currentWindow: true };
+  chrome.tabs.query(query, (tabs) => {
+    const currentTab = tabs[0];
+    chrome.browserAction.setPopup({ popup: "popupNotShop.html", tabId: currentTab.id });
+    chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id });
+    chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
+  });
+});
 
-// onInstalled has a details.reason, for the next update we need to use this value
-// to ensure this only runs when details.reason == "install"
-chrome.runtime.onInstalled.addListener(
-  function handleInstalled(details) {
-    let welcomeTabId;
+chrome.tabs.onUpdated.addListener(() => {
+  const query = { active: true, currentWindow: true };
+  chrome.tabs.query(query, (tabs) => {
+    const currentTab = tabs[0];
+    chrome.tabs.sendMessage(currentTab.id, { msgName: "reevaluatePage" });
+  });
+});
 
-    if (details.reason == "install") {
-      chrome.tabs.create({url: "https://ethicli.com/welcome"}, function(result) {
-        welcomeTabId = result.id;
+chrome.runtime.onInstalled.addListener((details) => {
+  let welcomeTabId;
 
-        chrome.tabs.onUpdated.addListener(
-          function() {
-            var query = { active: true, currentWindow: true };
-            chrome.tabs.query(query, function callback(tabs) {
-                var currentTab = tabs[0];
-                if (currentTab.id  === welcomeTabId) {
-                  chrome.tabs.sendMessage(currentTab.id, { msgName: "isEthicliWelcomePage" });
-                }
-            })
-          }
-        )
-      })
-    }
+  if (details.reason !== "install") {
+    return;
   }
-);
+  chrome.tabs.create({ url: "https://ethicli.com/welcome" }, (result) => {
+    welcomeTabId = result.id;
+
+    chrome.tabs.onUpdated.addListener(() => {
+      const query = { active: true, currentWindow: true };
+      chrome.tabs.query(query, (tabs) => {
+        const currentTab = tabs[0];
+        if (currentTab.id === welcomeTabId) {
+          chrome.tabs.sendMessage(currentTab.id, { msgName: "isEthicliWelcomePage" });
+        }
+      });
+    });
+  });
+});
 
 // GOOGLE ANALYTICS
 const GA_TRACKING_ID = "UA-173025073-1";
@@ -207,10 +176,10 @@ const GA_CLIENT_ID = "4FB5D5BF-B582-41AD-9BDF-1EC789AE6544";
 
 function reportGA(aType) {
   try {
-    let request = new XMLHttpRequest();
-    let message =
-      "v=1&tid=" + GA_TRACKING_ID + "&cid= " + GA_CLIENT_ID + "&aip=1" +
-      "&ds=add-on&t=event&ec=VISITORS&ea=" + aType;
+    const request = new XMLHttpRequest();
+    const message
+      = "v=1&tid=" + GA_TRACKING_ID + "&cid= " + GA_CLIENT_ID + "&aip=1"
+      + "&ds=add-on&t=event&ec=VISITORS&ea=" + aType;
     request.open("POST", "https://www.google-analytics.com/collect", true);
     request.send(message);
   } catch (e) {
--- Extension/popup-scripts/popup-optin.js
+++ Extension/popup-scripts/popup-optin.js
@@ -1,23 +1,19 @@
-window.addEventListener("load", function() {
-    document.getElementById("optinYes").addEventListener(
-      "click",
-      function() {
-        chrome.storage.local.set({ "optIn" : true }, function() {
-          var query = { active: true, currentWindow: true };
-          chrome.tabs.query(query, function callback(tabs) {
-            var currentTab = tabs[0];
-            chrome.tabs.sendMessage(currentTab.id, { msgName: "reevaluatePage" }, function(response) {
-              window.close()
-            })
-          })
+window.addEventListener("load", () => {
+  document.getElementById("optinYes").addEventListener("click", () => {
+    chrome.storage.local.set({ "optIn": true }, () => {
+      const query = { active: true, currentWindow: true };
+      chrome.tabs.query(query, (tabs) => {
+        const currentTab = tabs[0];
+        chrome.tabs.sendMessage(currentTab.id, { msgName: "reevaluatePage" }, (response) => {
+          window.close();
         });
+      });
     });
+  });
 
-    document.getElementById("optinNo").addEventListener(
-      "click",
-      function() {
-        chrome.storage.local.set({ "optIn" : false }, function() {
-          window.close()
-        });
+  document.getElementById("optinNo").addEventListener("click", () => {
+    chrome.storage.local.set({ "optIn": false }, () => {
+      window.close();
     });
+  });
 });
