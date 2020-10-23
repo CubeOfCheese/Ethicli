af2bebe12546358daa471b3827c8d1c849e8ac16
CubeOfCheese <cubeofcheese@gmail.com>
2020-10-23 08:16:50 -0700

    removes welcome from website since it is now in the extension instead

--- Website-and-Backend/src/main/java/com/example/appengine/springboot/Router.java
+++ Website-and-Backend/src/main/java/com/example/appengine/springboot/Router.java
@@ -23,11 +23,6 @@ public class Router {
     return "privacy";
   }
 
-  @RequestMapping(value = "/welcome")
-  public String getWelcome() {
-    return "welcome";
-  }
-
   @RequestMapping(value = "/info/{company}")
   public String getInfo(@PathVariable("company") String companyName, Model model) {
     model.addAttribute("companyName", companyName); // doesn't do anything as of right now. Leaving it in as an example for future thymeleaf use
