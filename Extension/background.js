9ad67e9030036fccd2b83cbfda4d7b0a6878468c
CubeOfCheese <cubeofcheese@gmail.com>
2020-11-25 09:21:45 -0800

    Replaces Google Analytics xmlhttprequest with a fetch. Not sure why it fixes the error but it does

--- Extension/background.js
+++ Extension/background.js
@@ -166,12 +166,15 @@ const GA_CLIENT_ID = "4FB5D5BF-B582-41AD-9BDF-1EC789AE6544";
 
 function reportGA(aType) {
   try {
-    const request = new XMLHttpRequest();
-    const message
-      = "v=1&tid=" + GA_TRACKING_ID + "&cid= " + GA_CLIENT_ID + "&aip=1"
-      + "&ds=add-on&t=event&ec=VISITORS&ea=" + aType;
-    request.open("POST", "https://www.google-analytics.com/collect", true);
-    request.send(message);
+    const url = "https://www.google-analytics.com/collect";
+    const message = `v=1&tid=${GA_TRACKING_ID}&cid=${GA_CLIENT_ID}&aip=1&ds=add-on&t=event&ec=VISITORS&ea=${aType}`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: message
+    });
   } catch (e) {
     console.error("Error sending report to Google Analytics.\n" + e);
   }
