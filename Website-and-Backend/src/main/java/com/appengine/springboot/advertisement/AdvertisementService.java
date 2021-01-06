efc2f7b63791da0f97d3c19ccb26cd69ace66f3d
Cosmo Stevens <justcosmo@ymail.com>
2021-01-06 10:33:58 -0800

    catch for null negativeProductTags

--- Website-and-Backend/src/main/java/com/appengine/springboot/advertisement/AdvertisementService.java
+++ Website-and-Backend/src/main/java/com/appengine/springboot/advertisement/AdvertisementService.java
@@ -110,12 +110,16 @@ public class AdvertisementService {
 
   public List<Advertisement> addAllAdvertisements(List<Advertisement> advertisements) {
     for (Advertisement advertisement : advertisements) {
-      for (int b = 0; b < advertisement.getProductTags().length; ++b) {
-        advertisement.getProductTags()[b].setTag(advertisement.getProductTags()[b].getTag().toLowerCase());
+      if (advertisement.getProductTags() != null) {
+        for (int b = 0; b < advertisement.getProductTags().length; ++b) {
+          advertisement.getProductTags()[b].setTag(advertisement.getProductTags()[b].getTag().toLowerCase());
+        }
       }
-      for (int b = 0; b < advertisement.getNegativeProductTags().length; ++b) {
-        advertisement.getNegativeProductTags()[b].setTag(advertisement.getNegativeProductTags()[b].getTag().toLowerCase());
-        advertisement.getNegativeProductTags()[b].setWeight(-1 * Math.abs(advertisement.getNegativeProductTags()[b].getWeight()));
+      if (advertisement.getNegativeProductTags() != null) {
+        for (int b = 0; b < advertisement.getNegativeProductTags().length; ++b) {
+          advertisement.getNegativeProductTags()[b].setTag(advertisement.getNegativeProductTags()[b].getTag().toLowerCase());
+          advertisement.getNegativeProductTags()[b].setWeight(-1 * Math.abs(advertisement.getNegativeProductTags()[b].getWeight()));
+        }
       }
     }
     return advertisementRepository.insert(advertisements);
