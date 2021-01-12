59431bf4ea7d406ef47041b509abcdbb1932b568
Linda <38482675+lindaicing@users.noreply.github.com>
2021-01-11 20:37:53 -0800

    Amys tools ad-adder(#508)
    Creates Amy's tool: ad adder
    Allows creation of an ad and submits it to database
    Makes it secure behind admin password
    
    Co-authored-by: CubeOfCheese <cubeofcheese@gmail.com>
    Co-authored-by: Cosmo Stevens <justcosmo@ymail.com>

--- Website-and-Backend/src/main/java/com/appengine/springboot/Router.java
+++ Website-and-Backend/src/main/java/com/appengine/springboot/Router.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.appengine.springboot.advertisement.Advertisement;
 
 @Controller
 public class Router {
@@ -58,5 +59,12 @@ public class Router {
   public String getInfo(@PathVariable("company") String companyName, Model model) throws IOException {
     model.addAttribute("business", businessService.getBusinessByWebsite(companyName));
     return "info";
+  }
+
+  @RequestMapping(value = "/amys-tools/ad-adder")
+  public String getAmyToolsAd(Model model) {
+    Advertisement Advertisement = new Advertisement();
+		model.addAttribute("Advertisement", Advertisement);
+    return "AmysTools/ad";
   }
 }
