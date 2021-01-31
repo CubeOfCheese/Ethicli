1c55a20bccfb900243ff720ffef5c8edd014d735
CubeOfCheese <cubeofcheese@gmail.com>
2021-01-30 16:50:43 -0800

    blocklisting unnecessary parameters in mixpanel

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,7 +1,22 @@
 import { getDomainWithoutSuffix } from "tldts-experimental";
 import mixpanel from "mixpanel-browser";
-mixpanel.init("db3fa3fa397bb591b339887d12b1c13e", { api_host: "https://api.mixpanel.com" });
-
+mixpanel.init("db3fa3fa397bb591b339887d12b1c13e",
+    { api_host: "https://api.mixpanel.com",
+      ip: false,
+      property_blacklist: [
+        "$city",
+        "$region",
+        "mp_country_code",
+        "$os",
+        "$browser_version",
+        "$current_url",
+        "$initial_referring_domain",
+        "$initial_referrer",
+        "$referrer",
+        "$search_engine"
+      ]
+    }
+);
 let hasSubscore;
 let fullheight = 0;
 
