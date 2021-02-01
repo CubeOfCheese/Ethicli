414f1b9ee559bc22fff9d11321b26c67bcf61032
Cosmo Stevens <justcosmo@ymail.com>
2021-02-01 11:21:19 -0800

    Removed All Manual Score CSV References (#539)
    * Removed All Manual Score CSV References
    
    * fix format
    
    * more format errors

--- Website-and-Backend/src/main/java/com/appengine/springboot/Tools.java
+++ Website-and-Backend/src/main/java/com/appengine/springboot/Tools.java
@@ -1,7 +1,6 @@
 package com.appengine.springboot;
 
 import com.appengine.springboot.advertisement.ProductTag;
-import com.appengine.springboot.business.Business;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,43 +23,6 @@ public class Tools {
     return output;
   }
 
-  // Compares searchTerm with business.website if it exists, if no match is found, it compares with
-  // business.name
-  public static boolean businessCompare(String searchTerm, Business business) {
-    searchTerm = searchTerm.toLowerCase();
-    if (business.getWebsite() != null) { // Compares searchTerm with business.website
-      String website = business.getWebsite().toLowerCase();
-      if (website.contains("http://")) {
-        website = website.substring(7);
-      } else if (website.contains("https://")) {
-        website = website.substring(8);
-      }
-      if (website.contains("www.")) {
-        website = website.substring(4);
-      }
-      if (website.length() > searchTerm.length()) {
-        if (searchTerm.equals(website.substring(0, searchTerm.length()))
-            && website.charAt(searchTerm.length()) == '.') {
-          return true;
-        }
-      }
-    }
-    // Compare searchTerm with business.name - Checks matches of words in business.name individually
-    // and together
-    String[] nameArray = prepareBusinessSearchTerm(business.getName().toLowerCase()).split(" ");
-    String nameToken;
-    for (int a = 0; a < nameArray.length; ++a) {
-      nameToken = "";
-      for (int b = a; b < nameArray.length; ++b) {
-        nameToken += nameArray[b];
-        if (searchTerm.equals(nameToken)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
   // Counts number of " in a String
   public static int doubleQuoteCounter(String content) {
     int counter = 0;
@@ -70,42 +32,6 @@ public class Tools {
       }
     }
     return counter;
-  }
-
-  // Converts rows in a csv fit to string arrays
-  public static String[] csvToStringArray(String content) {
-    String[] splitByComma;
-    String output = "";
-    boolean quoteDetected = false;
-    content = content.replaceAll("\"\"", "@@DOUBLEQUOTES@@");
-    splitByComma = content.split(",");
-    for (int a = 0; a < splitByComma.length; ++a) {
-      if (splitByComma[a].contains("\"")) {
-        if (doubleQuoteCounter(splitByComma[a]) == 1) {
-          quoteDetected = !quoteDetected;
-        }
-      }
-      if (quoteDetected) {
-        output += splitByComma[a] + ",";
-      } else {
-        output += splitByComma[a] + "@@DELIMITER@@";
-      }
-    }
-    return output.replaceAll("\"", "").replaceAll("@@DOUBLEQUOTES@@", "\"").split("@@DELIMITER@@");
-  }
-
-  // Removes unnecessary characters from searchTerm
-  public static String prepareBusinessSearchTerm(String searchTerm) {
-    searchTerm = searchTerm.toLowerCase();
-    searchTerm = charRemove(searchTerm, ',');
-    searchTerm = charRemove(searchTerm, '\'');
-    searchTerm = charRemove(searchTerm, ':');
-    searchTerm = charRemove(searchTerm, ';');
-    searchTerm = charRemove(searchTerm, '.');
-    searchTerm = charRemove(searchTerm, '-');
-    searchTerm = charRemove(searchTerm, '_');
-    searchTerm = charRemove(searchTerm, '&');
-    return searchTerm;
   }
 
   public static String[] prepareForProductTagQuery(String input) throws IOException {
