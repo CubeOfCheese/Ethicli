8832138c7825506f3b46e2859c92db0808fb1568
Cosmo Stevens <justcosmo@ymail.com>
2021-02-04 12:27:27 -0800

    Remove Tools File

--- Website-and-Backend/src/main/java/com/appengine/springboot/business/BusinessService.java
+++ Website-and-Backend/src/main/java/com/appengine/springboot/business/BusinessService.java
@@ -1,9 +1,14 @@
 package com.appengine.springboot.business;
 
-import com.appengine.springboot.Tools;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -22,7 +27,7 @@ public class BusinessService {
 
   public Business getBusinessByWebsite(String companyName) throws IOException {
     Business business = new Business();
-    if (Tools.validateURL(companyName)) {
+    if (validateURL(companyName)) {
       List<Business> businessList = regexWebsite(companyName);
       for (Business value : businessList) {
         if (companyName.equals(value.getWebsite().split("\\.")[0])) {
@@ -34,4 +39,23 @@ public class BusinessService {
     }
     return business;
   }
+
+	// Compares companyName to Blocked URLS - Sheet.csv
+	public static boolean validateURL(String companyName) throws IOException {
+		Resource resource = new ClassPathResource("Blocked URLS - Sheet1.csv");
+		InputStream file = resource.getInputStream();
+		BufferedReader br = null;
+		String line = "";
+		try {
+			br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
+			while ((line = br.readLine()) != null) { // cycles through line by line
+				if (companyName.equals(line)) {
+					return false;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 }
