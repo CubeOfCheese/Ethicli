1c6d69d9a1edbe8e0d8367161df6eb520e7eb0bb
Cosmo Stevens <justcosmo@ymail.com>
2021-02-04 17:50:01 -0800

    remove unused code and blocked url file

--- Website-and-Backend/src/main/java/com/appengine/springboot/business/BusinessService.java
+++ Website-and-Backend/src/main/java/com/appengine/springboot/business/BusinessService.java
@@ -1,14 +1,8 @@
 package com.appengine.springboot.business;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -25,37 +19,16 @@ public class BusinessService {
     return mongoOperations.find(query, Business.class);
   }
 
-  public Business getBusinessByWebsite(String companyName) throws IOException {
-    Business business = new Business();
-    if (validateURL(companyName)) {
-      List<Business> businessList = regexWebsite(companyName);
-      for (Business value : businessList) {
-        if (companyName.equals(value.getWebsite().split("\\.")[0])) {
-          business.update(value);
-          break;
-        }
-      }
-      business.calculate();
-    }
-    return business;
-  }
-
-	// Compares companyName to Blocked URLS - Sheet.csv
-	public static boolean validateURL(String companyName) throws IOException {
-		Resource resource = new ClassPathResource("Blocked URLS - Sheet1.csv");
-		InputStream file = resource.getInputStream();
-		BufferedReader br = null;
-		String line = "";
-		try {
-			br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
-			while ((line = br.readLine()) != null) { // cycles through line by line
-				if (companyName.equals(line)) {
-					return false;
-				}
+	public Business getBusinessByWebsite(String companyName) throws IOException {
+		Business business = new Business();
+		List<Business> businessList = regexWebsite(companyName);
+		for (Business value : businessList) {
+			if (companyName.equals(value.getWebsite().split("\\.")[0])) {
+				business.update(value);
+				break;
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-		return true;
+		business.calculate();
+		return business;
 	}
 }
