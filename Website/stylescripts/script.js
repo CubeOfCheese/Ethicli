621e48713cdd29b895962b66f2695b6bd3d8fc74
Linda <lindathehuang@gmail.com>
2021-03-07 19:14:32 -0800

    Removed touchscreen detection

--- Website/stylescripts/script.js
+++ Website/stylescripts/script.js
@@ -92,36 +92,10 @@ $(document).ready(function() {
 
   dropdown();
   function dropdown() {
-    let hasTouchScreen = false;
-    if ("maxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.maxTouchPoints > 0;
-    } else if ("msMaxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.msMaxTouchPoints > 0;
-    } else {
-      const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-      if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-      } else if ("orientation" in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-      } else { // Only as a last resort, fall back to user agent sniffing
-        const UA = navigator.userAgent;
-        hasTouchScreen = (
-          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA)
-            || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-      }
-    }
-    if (hasTouchScreen) {
-      $(".dropdownMenu").click(function() {
-        $(".dropdownIcon").toggleClass("dropped");
-        $(".submenu").toggleClass("submenuClicked");
-      });
-    } else {
-      $(".dropdownMenu").hover(function() {
-        $(".dropdownIcon").toggleClass("dropped");
-        $(".submenu").toggleClass("submenuClicked");
-      });
-    }
+    $(".dropdownMenu").hover(function() {
+      $(".dropdownIcon").toggleClass("dropped");
+      $(".submenu").toggleClass("submenuClicked");
+    });
   }
 
   mobileNav(); // allows menu to toggle when starting at a mobile size
