9d0b03716e1ef397cd26882c747f3343f8f115ae
Linda <lindathehuang@gmail.com>
2021-03-28 23:30:17 -0700

    404 test netlify

--- Website/stylescripts/script.js
+++ Website/stylescripts/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   $(function() { // Load Nav and Footer
     $("#nav").load("./components/nav.html");
-    $("#nav_404").load("./views/components/nav.html");
     $("#footer").load("./components/footer.html");
-    $("#footer_404").load("./views/components/footer.html");
   });
 
   $("#currentYear").text(new Date().getFullYear()); // updates copyright with current year
