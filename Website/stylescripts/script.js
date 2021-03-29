e15bd5c053d4312b833d6cb22d4c9303e7164677
Linda <lindathehuang@gmail.com>
2021-03-28 22:58:08 -0700

    Netlify test

--- Website/stylescripts/script.js
+++ Website/stylescripts/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $(function() { // Load Nav and Footer
-    $("#nav").load("../components/nav.html");
-    $("#nav_404").load("./components/nav.html");
-    $("#footer").load("../components/footer.html");
-    $("#footer_404").load("./components/footer.html");
-  });
-  $("#currentYear").text(new Date().getFullYear()); // updates copyright with current year
+  // $(function() { // Load Nav and Footer
+  //   $("#nav").load("../components/nav.html");
+  //   $("#nav_404").load("./components/nav.html");
+  //   $("#footer").load("../components/footer.html");
+  //   $("#footer_404").load("./components/footer.html");
+  // });
+  // $("#currentYear").text(new Date().getFullYear()); // updates copyright with current year
 });
