7bc1d05d32facbad7946b8a091bde9e1435b84e5
Billy T. Hernandez <cubeofcheese@gmail.com>
2021-06-12 20:10:45 -0700

    Refactor background.js to eliminate redundant calls to backend (#676)
    * removes unused isShoppingPage variable
    * refactoring of tab id references to reduce complexity
    * Stores score for tab instead of requiring re-fetch of data
    * onUpdated made unnecessary by using sender.tab.id instead of active tab id

--- Extension/background.js
+++ Extension/background.js
@@ -1,66 +1,68 @@
 import config from "../config/config.js";
-import { getDomainWithoutSuffix } from "tldts-experimental";
+import { getDomainWithoutSuffix, getDomain } from "tldts-experimental";
 import mixpanel from "mixpanel-browser";
 mixpanel.init(config.mixpanel_code, config.mixpanel_config);
 
 chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" } });
 
-let isShoppingPage;
 let ethicliStats;
 let productName;
 
+// Feb 23 2021
+// etsy is included because we don't have a way to prevent matching of etsy with etsy.com/exampleShop
+// so etsy is awarded badges that should actually be attributed to exampleShop
+const blocklist = [
+  "google",
+  "bing",
+  "yahoo",
+  "baidu",
+  "aol",
+  "duckduckgo",
+  "yandex",
+  "ecosia",
+  "etsy",
+  "youtube",
+  "facebook",
+  "instagram"
+];
+
 function notShop(currentTab) {
-  isShoppingPage = false;
   chrome.browserAction.setPopup({ popup: "views/popupNotShop.html", tabId: currentTab.id });
   chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id });
   chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
 }
 
 function reloadExt(request, sender) {
-  const query = { active: true, currentWindow: true };
-  chrome.tabs.query(query, (tabs) => {
-    const currentTab = tabs[0];
+  if (!request.shoppingPage) {
+    notShop(sender.tab.id);
+    return;
+  }
 
-    if (!request.shoppingPage) {
-      notShop(currentTab);
+  chrome.storage.local.get([ sender.tab.id.toString() ], (jsonResponse) => {
+    if (jsonResponse[sender.tab.id] && getDomain(sender.tab.url) === jsonResponse[sender.tab.id].website.split("/")[0]) { // check storage
+      ethicliStats = jsonResponse[sender.tab.id];
+      let ethicliBadgeScore = Math.round(ethicliStats.overallScore);
+      if ((isNaN(ethicliStats.overallScore)) || (ethicliBadgeScore === 0)) {
+        ethicliBadgeScore = "";
+        chrome.browserAction.setPopup({ popup: "views/popupNoRating.html", tabId: sender.tab.id });
+      } else {
+        chrome.browserAction.setPopup({ popup: "views/popupShop.html", tabId: sender.tab.id });
+      }
+      chrome.browserAction.setIcon({ path: { "16": "icons/ethicli-16.png" }, tabId: sender.tab.id });
+      chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: sender.tab.id });
       return;
     }
-    chrome.browserAction.setIcon({ path: { "16": "icons/ethicli-16.png" }, tabId: currentTab.id });
-    isShoppingPage = true;
-
+    // not found in storage
     const companyName = getDomainWithoutSuffix(sender.tab.url);
 
-    // Feb 23 2021
-    // etsy is included because we don't have a way to prevent matching of etsy with etsy.com/exampleShop
-    // so etsy is awarded badges that should actually be attributed to exampleShop
-    const blocklist = [
-      "google",
-      "bing",
-      "yahoo",
-      "baidu",
-      "aol",
-      "duckduckgo",
-      "yandex",
-      "ecosia",
-      "etsy",
-      "youtube",
-      "facebook",
-      "instagram"
-    ];
-    let isBlocklisted;
     let ethicliBadgeScore;
+
     for (let b = 0; b < blocklist.length; b++) {
       if (companyName.includes(blocklist[b])) {
         ethicliBadgeScore = "";
-        isBlocklisted = true;
-        break;
-      } else {
-        isBlocklisted = false;
+        notShop(sender.tab.id);
+        return;
       }
-    }
-    if (isBlocklisted) {
-      notShop(currentTab);
-      return;
     }
 
     const url = "https://info.ethicli.com/score/" + companyName;
@@ -71,29 +73,28 @@ function reloadExt(request, sender) {
 
           if ((isNaN(jsonResponse.overallScore)) || (ethicliBadgeScore === 0)) {
             ethicliBadgeScore = "";
-            chrome.browserAction.setPopup({ popup: "views/popupNoRating.html", tabId: currentTab.id });
+            chrome.browserAction.setPopup({ popup: "views/popupNoRating.html", tabId: sender.tab.id });
             mixpanel.track("Visit shop", {
               "Has score": false,
               "Shop words": request.shopWords
             });
           } else {
-            chrome.browserAction.setPopup({ popup: "views/popupShop.html", tabId: currentTab.id });
+            chrome.browserAction.setPopup({ popup: "views/popupShop.html", tabId: sender.tab.id });
+            chrome.storage.local.set({ [sender.tab.id.toString()]: jsonResponse });
             mixpanel.track("Visit shop", {
               "Has score": true,
               "Shop words": request.shopWords
             });
           }
-          chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: currentTab.id });
+          chrome.browserAction.setBadgeText({ text: ethicliBadgeScore.toString(), tabId: sender.tab.id });
         });
+    chrome.browserAction.setIcon({ path: { "16": "icons/ethicli-16.png" }, tabId: sender.tab.id });
   });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.msgName) {
-    case "isShoppingPage?":
-      sendResponse({ shoppingPage: isShoppingPage });
-      break;
-    case "whatsMainRating?":
+    case "whatsMainRating?": // needs to be refactored. Using global ethicliStats variable is causing the quick tab switch bug
       sendResponse({ ethicliStats: ethicliStats });
       break;
     case "productIdentified?":
@@ -115,7 +116,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-chrome.tabs.onActivated.addListener(() => {
+chrome.tabs.onActivated.addListener(() => { // tab switch
   const query = { active: true, currentWindow: true };
   chrome.tabs.query(query, (tabs) => {
     const currentTab = tabs[0];
@@ -123,17 +124,17 @@ chrome.tabs.onActivated.addListener(() => {
       // On first page visit, response is null. This function is only supposed to run after the first visit anyway,
       // so this just gets rid of an error that didn't actually break anything
       if (response) {
-        isShoppingPage = response.isShoppingPage;
         // currentTab.url is null for new tab pages when first opened
         // and equal to "chrome://newtab/" when navigated to from another tab.
-        if (isShoppingPage && currentTab.url && (currentTab.url !== "chrome://newtab/")) {
+        if (response.isShoppingPage && currentTab.url && (currentTab.url !== "chrome://newtab/")) {
           // data should only be retrieved for actual pages that are shopping pages
-          chrome.tabs.sendMessage(tabs[0].id, { msgName: "productIdentified?" }, (response) => {
+          chrome.tabs.sendMessage(currentTab.id, { msgName: "productIdentified?" }, (response) => {
             productName = response.productName;
           });
           const request = { msgName: "PageEvaluated", shoppingPage: true };
           const sender = { tab: { url: "" } };
           sender.tab.url = currentTab.url;
+          sender.tab.id = currentTab.id;
           reloadExt(request, sender);
         }
       }
@@ -148,14 +149,6 @@ chrome.tabs.onCreated.addListener(() => {
     chrome.browserAction.setPopup({ popup: "views/popupNotShop.html", tabId: currentTab.id });
     chrome.browserAction.setIcon({ path: { "16": "icons/grey-16.png" }, tabId: currentTab.id });
     chrome.browserAction.setBadgeText({ text: "", tabId: currentTab.id });
-  });
-});
-
-chrome.tabs.onUpdated.addListener(() => {
-  const query = { active: true, currentWindow: true };
-  chrome.tabs.query(query, (tabs) => {
-    const currentTab = tabs[0];
-    chrome.tabs.sendMessage(currentTab.id, { msgName: "reevaluatePage" });
   });
 });
 
