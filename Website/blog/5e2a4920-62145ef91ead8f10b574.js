d25fb47789452f8f6b153f020be457108938777a
CubeOfCheese <cubeofcheese@gmail.com>
2021-06-17 08:15:27 -0700

    hugo blog attempt 1

--- Website/blog/5e2a4920-62145ef91ead8f10b574.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{Tgqd:function(t,n,e){"use strict";e.d(n,"a",(function(){return i}));var r=e("Lnxd"),i=function(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"line",attr:{x1:"3",y1:"12",x2:"21",y2:"12"}},{tag:"line",attr:{x1:"3",y1:"6",x2:"21",y2:"6"}},{tag:"line",attr:{x1:"3",y1:"18",x2:"21",y2:"18"}}]})(t)};i.displayName="FiMenu"}}]);
-//# sourceMappingURL=5e2a4920-62145ef91ead8f10b574.js.map
---
+++ Website/blog/content/posts/my-first-post.md
@@ -0,0 +1,8 @@
+---
+title: "My First Post"
+date: 2021-06-17T07:54:55-07:00
+draft: false
+featured_image: "./images/esmeralda.jpg"
+show_reading_time: true
+---
+hi
